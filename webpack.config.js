0f3b23c9eb492747bd5548df86fc19b9075fc58f
Nicolas Straub <spameggspambakedbeansandspam@gmail.com>
2016-12-16 06:00:37 -0300

    rename out file to conform to react-redux naming scheme

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: './build',
-        filename: 'simple-react-redux.js'
+        path: './lib',
+        filename: 'index.js'
     },
     module: {
         loaders: [
